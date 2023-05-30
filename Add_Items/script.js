08e108f10ad54e0cc86e25aadc049808a1619f4f
EagleVector <kumarsubhambili3@gmail.com>
2023-05-30 13:50:19 +0530

    filter items and clearing the UI state using DOM in vanilla JS

--- Add_Items/script.js
+++ Add_Items/script.js
@@ -2,6 +2,8 @@ const itemform = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+const items = itemList.querySelectorAll('li');
 
 function addItem(e) {
   e.preventDefault();
@@ -11,13 +13,18 @@ function addItem(e) {
     alert('Please add an item');
     return;
   }
+
+  // create list item
   const li = document.createElement('li');
   li.appendChild(document.createTextNode(newItem));
 
   const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
 
+  // Add li to the DOM
   itemList.appendChild(li);
+
+  checkUI();
   itemInput.value = '';
 
 }
@@ -38,16 +45,53 @@ function createIcon(classes) {
 
 function removeItem(e) {
   if (e.target.parentElement.classList.contains('remove-item')) {
-    e.target.parentElement.parentElement.remove();
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+
+      checkUI();
+    }
   }
 }
 
 function clearItems() {
-  while (itemList.firstChild) {
-    itemList.removeChild(itemList.firstChild);
+  if (confirm('Clear the cart?')) {
+    while (itemList.firstChild) {
+      itemList.removeChild(itemList.firstChild);
+    }
+  }
+
+  checkUI();
+}
+
+function filterItems(e) {
+  const items = itemList.querySelectorAll('li');
+  const text = e.target.value.toLowerCase();
+
+  items.forEach(item => {
+    const itemName = item.firstChild.textContent.toLocaleLowerCase();
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none'
+    }
+  })
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll('li');
+  if (items.length === 0) {
+    clearBtn.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearBtn.style.display = 'block';
+    itemFilter.style.display = 'block';
   }
 }
 
 itemform.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
+
+checkUI();
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Simple shopping list app using javascript.
 ## 1. Add items to the list
 ## 2. Clear items with x button
+## 3. Clear UI state
+## 4. Filter items
+## 5. Local Storage
